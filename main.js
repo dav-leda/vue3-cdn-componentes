8a3ab113b384f54d32c0cc1a6ec360dfa1ece6d8
David Leda <davmail99@yahoo.com>
2023-05-27 05:37:02 -0300

    custom directives

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 import App from './app.js'
 import * as customDirectives from './directives/customDirectives.js'
 
+// create(app).mount('#app')
+
+// Declarar custom directives antes del mount:
 createApp(App)
   .directive('happy', customDirectives.happy)
   .directive('sad', customDirectives.sad)
