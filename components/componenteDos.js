578563a6f55745f967535d0be24225700eb5ee33
David Leda <davmail99@yahoo.com>
2023-05-30 19:36:50 -0300

    props

--- components/componenteDos.js
+++ components/componenteDos.js
@@ -4,6 +4,13 @@ export default {
     prop2: String
   },
 
+  // Si declaro data con el mismo nombre de la prop
+  // el contenido de data pisa la prop
+  data: () => ({
+    prop2: 'otra cosa'
+  }),
+
+
   template: /*html*/ `
 
     <div class="p-1 m-1 border rounded">
