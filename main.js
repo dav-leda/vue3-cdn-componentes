8fa986e9d2922d69fac954d421e819620b18d890
David Leda <davmail99@yahoo.com>
2023-05-30 04:15:00 -0300

    componentes

--- main.js
+++ main.js
@@ -5,17 +5,7 @@ import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 // Importar el componente raíz de la aplicación (App):
 import App from './app.js'
 
-// Importar directivas "custom" (esto lo pueden omitir):
-import * as customDirectives from './directives/customDirectives.js'
-
-// Al usar componentes, la forma más correcta de declarar
-// el método createApp es así:
-
-// createApp(App).mount('#app')
-
-// Si se usan directivas "custom", declararlas antes del mount:
-createApp(App)
-  .directive('happy', customDirectives.happy)
-  .directive('sad', customDirectives.sad)
-  .mount('#app')
+// Al usar componentes la forma recomendada
+// para montar la aplicación es esta:
+createApp(App).mount('#app')
 
