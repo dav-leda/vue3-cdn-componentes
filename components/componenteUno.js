2dfb9ed1bc121172d3af2f38c6e1eb3856d3c886
David Leda <davmail99@yahoo.com>
2023-06-01 15:49:15 -0300

    no slots

--- components/componenteUno.js
+++ components/componenteUno.js
@@ -17,14 +17,12 @@ export default {
   
   // Resaltado de sintaxis con la extensión Inline HTML para VS Code
   // Hay que poner /*html*/ antes del backtick: 
-  
+
   template: /*html*/ `
 
     <div class="m-2 p-1 border rounded center">
 
       <h1>Componente 1</h1>
-
-      <slot/>
     
       <componente-dos :prop2="prop1"/>
       <ComponenteDos :prop2="prop1"/>
